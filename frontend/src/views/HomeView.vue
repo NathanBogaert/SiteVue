8ecc8f3e268ded1807afdc49a0e946484bbf53f2
NathanBogaert <nathanbogaert59@gmail.com>
2022-12-15 08:22:57 +0100

    Ajout bouton home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,31 +1,57 @@
 <template>
-  <div class="home">
-    <ul v-for="quizz in quizzs" :key="quizz.name">
-        <li>
-            <router-link to="/question/:quizz.name">
-              {{ quizz.name }}
-              {{ quizz.description }}
-            </router-link>
-        </li>
-    </ul>
-  </div>
+    <div id="linkButton">
+        <router-link to="/create">
+          <font-awesome-icon icon="plus"/>
+          <span>Créer un quizz</span>
+        </router-link>
+        <router-link to="/collection">
+          <font-awesome-icon icon="rectangle-list"/>
+          <span>Voir vos quizz</span>
+        </router-link>
+    </div>
+    <div class="home">
+      <ul v-for="quizz in quizzs" :key="quizz.name">
+          <li>
+              <router-link to="/question/:quizz.name">
+                {{ quizz.name }}
+                {{ quizz.description }}
+              </router-link>
+          </li>
+      </ul>
+    </div>
 </template>
 
 <script>
 export default {
-  data() {
+data() {
     return {
         quizzs: []
     }
-  },
-  async created() {
+},
+async created() {
     const response = await fetch("/quizz");
     this.quizz = await response.json();
-  }
+}
 }
 </script>
 
 <style scoped>
+#linkButton > a {
+    background-color: hsl(0, 0%, 98%);
+    color: #414141;
+    text-decoration: none;
+    padding: 1% 2%;
+    margin: 2% 4%;
+    border-radius: 40px;
+    transition: .5s;
+}
+#linkButton > a:hover {
+    transition: .5s;
+    background-color: #42b983;
+}
+#linkButton > a > svg {
+    margin-right: 1%;
+}
 ul {
     list-style-type: none;
     padding: 0;
